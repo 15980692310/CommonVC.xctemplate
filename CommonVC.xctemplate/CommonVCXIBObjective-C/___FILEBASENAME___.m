c6ee074a24b3c201288b0efe6268d2a6d99f0b0d
ZhangCC <15980692310@163.com>
2017-04-14 11:41:03 +0800

    1.0
    上传代码

---
+++ CommonVC.xctemplate/CommonVCXIBObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,42 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - Init Methods
+
+#pragma mark - Life cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+#pragma mark - Custom views
+
+#pragma mark - Event response
+
+#pragma mark - Notifications
+
+#pragma mark - UITableViewDelegate
+
+#pragma mark - XXXDelegate
+
+#pragma mark - Setter Getter Methods
+
+#pragma mark - Public methods
+
+#pragma mark - Private methods
+
+@end
